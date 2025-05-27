dec3a8b15c0a36419fcc772d6907f40b2a16bbdb
Neil van Rensburg <neil.vanrensburg@gmail.com>
2025-05-27 12:00:43 +0200

    Remove .idea, build, and .gradle directories from tracking

--- build/assets/webjars/jquery/3.5.1/webjars-requirejs.unminified.js
+++
@@ -1,9 +0,0 @@
-var process = process || {env: {NODE_ENV: "development"}};
-/*global requirejs */
-
-// Ensure any request for this webjar brings in jQuery.
-requirejs.config({
-    paths: { "jquery": webjars.path("jquery", "jquery") },
-    shim: { "jquery": { "exports": "$" } }
-});
-
