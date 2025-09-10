5e8408b5469108c60d14399952d52677a7b6454f
Sandro Cicero <sandro.cicero@loonar.cloud>
2025-09-10 05:07:14 -0300

    Remove @pam from SSH and SFTP operations

--- src/main/groovy/com/morpheusdata/proxmox/ve/util/ProxmoxMiscUtil.groovy
+++ src/main/groovy/com/morpheusdata/proxmox/ve/util/ProxmoxMiscUtil.groovy
@@ -14,71 +14,83 @@ import groovy.util.logging.Slf4j
 @Slf4j
 class ProxmoxMiscUtil {
 
-
-    static void sftpUpload(String host, int port, String username, String password, String localPath, String destDir, String privateKeyPath) {
-        JSch jsch = new JSch()
-        Session session = null
-        Channel channel = null
-        ChannelSftp channelSftp = null
-
-        try {
-            // Set up the private key if provided
-            if (privateKeyPath) {
-                jsch.addIdentity(privateKeyPath)
-            }
-
-            // Open a session to the remote server
-            session = jsch.getSession(username, host, port)
-            session.setConfig("StrictHostKeyChecking", "no") // Not recommended for production
-            session.setPassword(password)
-            session.connect()
-
-            // Open the SFTP channel
-            channel = session.openChannel("sftp")
-            channel.connect()
-            channelSftp = channel as ChannelSftp
-
-            // Upload file
-            String fileName = new File(localPath).getName()
-            String remoteFilePath = destDir.endsWith("/") ? destDir + fileName : destDir + "/" + fileName;
-            long startTime = System.currentTimeMillis();
-            channelSftp.put(localPath, remoteFilePath)
-            long endTime = System.currentTimeMillis()
-            long duration = (endTime - startTime) / 1000
-            log.debug("File uploaded: $localPath to $remoteFilePath in $duration seconds")
-
-        } catch (Exception e) {
-            e.printStackTrace()
-        } finally {
-            if (channelSftp != null) {
-                channelSftp.exit()
-            }
-            if (channel != null) {
-                channel.disconnect()
-            }
-            if (session != null) {
-                session.disconnect()
-            }
+    /**
+     * Remove '@pam' from SSH username if present
+     */
+    static String sanitizeSshUsername(String username) {
+        if (username?.endsWith('@pam')) {
+            return username.replace('@pam', '')
         }
+        return username
+    }
+    static void sftpUpload(String host, int port, String username, String password, String localPath, String destDir, String privateKeyPath) {
+            JSch jsch = new JSch()
+            Session session = null
+            Channel channel = null
+            ChannelSftp channelSftp = null
+
+            try {
+                // Set up the private key if provided
+                if (privateKeyPath) {
+                    jsch.addIdentity(privateKeyPath)
+                }
+
+                // Sanitize username for SSH
+                String sshUser = sanitizeSshUsername(username)
+                // Open a session to the remote server
+                session = jsch.getSession(sshUser, host, port)
+                session.setConfig("StrictHostKeyChecking", "no") // Not recommended for production
+                session.setPassword(password)
+                session.connect()
+
+                // Open the SFTP channel
+                channel = session.openChannel("sftp")
+                channel.connect()
+                channelSftp = channel as ChannelSftp
+
+                // Upload file
+                String fileName = new File(localPath).getName()
+                String remoteFilePath = destDir.endsWith("/") ? destDir + fileName : destDir + "/" + fileName;
+                long startTime = System.currentTimeMillis();
+                channelSftp.put(localPath, remoteFilePath)
+                long endTime = System.currentTimeMillis()
+                long duration = (endTime - startTime) / 1000
+                log.debug("File uploaded: $localPath to $remoteFilePath in $duration seconds")
+
+            } catch (Exception e) {
+                e.printStackTrace()
+            } finally {
+                if (channelSftp != null) {
+                    channelSftp.exit()
+                }
+                if (channel != null) {
+                    channel.disconnect()
+                }
+                if (session != null) {
+                    session.disconnect()
+                }
+            }
     }
 
 
     static void sftpCreateFile(String host, int port, String username, String password, String destFilePath, String content, String privateKeyPath) {
-        JSch jsch = new JSch()
-        Session session = null
-        Channel channel = null
-        ChannelSftp channelSftp = null
+            JSch jsch = new JSch()
+            Session session = null
+            Channel channel = null
+            ChannelSftp channelSftp = null
 
-        try {
-            // Set up the private key if provided
-            if (privateKeyPath) {
-                jsch.addIdentity(privateKeyPath)
-            }
+            try {
+                // Set up the private key if provided
+                if (privateKeyPath) {
+                    jsch.addIdentity(privateKeyPath)
+                }
 
-            // Open a session to the remote server
-            session = jsch.getSession(username, host, port)
-            session.setConfig("StrictHostKeyChecking", "no") // Not recommended for production
-            session.setPassword(password)
+                // Sanitize username for SSH
+                String sshUser = sanitizeSshUsername(username)
+                // Open a session to the remote server
+                session = jsch.getSession(sshUser, host, port)
+                session.setConfig("StrictHostKeyChecking", "no") // Not recommended for production
+                session.setPassword(password)
             session.connect()
 
             // Open the SFTP channel
--- src/main/groovy/com/morpheusdata/proxmox/ve/util/ProxmoxSshUtil.groovy
+++ src/main/groovy/com/morpheusdata/proxmox/ve/util/ProxmoxSshUtil.groovy
@@ -21,23 +21,30 @@ class ProxmoxSshUtil {
     static String IMAGE_PATH_PREFIX = "/var/opt/morpheus/morpheus-ui/vms/morpheus-images"
     static String REMOTE_IMAGE_DIR = "/var/lib/vz/template/qemu"
 
+    /**
+     * Remove '@pam' from SSH username if present
+     */
+    static String sanitizeSshUsername(String username) {
+        if (username?.endsWith('@pam')) {
+            return username.replace('@pam', '')
+        }
+        return username
+    }
+
 
 
     static void createCloudInitDrive(MorpheusContext context, ComputeServer hvNode, WorkloadRequest workloadRequest, String vmId, String datastoreId) {
         log.debug(log.debug("Configuring Cloud-Init"))
         log.debug("Ensuring snippets directory on node: $hvNode.externalId")
-        //context.executeSshCommand(hvNode.sshHost, 22, hvNode.sshUsername, hvNode.sshPassword, "mkdir -p /var/lib/vz/snippets", "", "", "", false, LogLevel.info, true, null, false).blockingGet()
         runSshCmd(context, hvNode, "mkdir -p /var/lib/vz/snippets")
         log.debug("Creating cloud-init user-data file on hypervisor node: /var/lib/vz/snippets/$vmId-cloud-init-user-data.yml")
-        ProxmoxMiscUtil.sftpCreateFile(hvNode.sshHost, 22, hvNode.sshUsername, hvNode.sshPassword, "/var/lib/vz/snippets/$vmId-cloud-init-user-data.yml", workloadRequest.cloudConfigUser, null)
+        ProxmoxMiscUtil.sftpCreateFile(hvNode.sshHost, 22, sanitizeSshUsername(hvNode.sshUsername), hvNode.sshPassword, "/var/lib/vz/snippets/$vmId-cloud-init-user-data.yml", workloadRequest.cloudConfigUser, null)
         log.debug("Creating cloud-init user-data file on hypervisor node: /var/lib/vz/snippets/$vmId-cloud-init-network.yml")
-        ProxmoxMiscUtil.sftpCreateFile(hvNode.sshHost, 22, hvNode.sshUsername, hvNode.sshPassword, "/var/lib/vz/snippets/$vmId-cloud-init-network.yml", workloadRequest.cloudConfigNetwork, null)
+        ProxmoxMiscUtil.sftpCreateFile(hvNode.sshHost, 22, sanitizeSshUsername(hvNode.sshUsername), hvNode.sshPassword, "/var/lib/vz/snippets/$vmId-cloud-init-network.yml", workloadRequest.cloudConfigNetwork, null)
         log.debug("Creating cloud-init vm disk: $datastoreId:cloudinit")
-        //context.executeSshCommand(hvNode.sshHost, 22, hvNode.sshUsername, hvNode.sshPassword, "qm set $vmId --ide2 $targetDSs[0].datastore.externalId:cloudinit", "", "", "", false, LogLevel.info, true, null, false).blockingGet()
         runSshCmd(context, hvNode, "qm set $vmId --ide2 $datastoreId:cloudinit")
         log.debug("Mounting cloud-init data to disk...")
         String ciMountCommand = "qm set $vmId --cicustom \"user=local:snippets/$vmId-cloud-init-user-data.yml,network=local:snippets/$vmId-cloud-init-network.yml\""
-        //context.executeSshCommand(hvNode.sshHost, 22, hvNode.sshUsername, hvNode.sshPassword, ciMountCommand, "", "", "", false, LogLevel.info, true, null, false).blockingGet()
         runSshCmd(context, hvNode, ciMountCommand)
     }
 
@@ -53,12 +60,12 @@ class ProxmoxSshUtil {
 
             //create qcow2 template directory on proxmox
             log.debug("Ensuring Image Directory on node: $hvNode.sshHost")
-            def dirOut = context.executeSshCommand(hvNode.sshHost, 22, hvNode.sshUsername, hvNode.sshPassword, "mkdir -p $REMOTE_IMAGE_DIR", "", "", "", false, LogLevel.info, true, null, false).blockingGet()
+                def dirOut = context.executeSshCommand(hvNode.sshHost, 22, sanitizeSshUsername(hvNode.sshUsername), hvNode.sshPassword, "mkdir -p $REMOTE_IMAGE_DIR", "", "", "", false, LogLevel.info, true, null, false).blockingGet()
             log.debug("Dir create SSH Task \"mkdir -p $REMOTE_IMAGE_DIR\" results: ${dirOut.toMap().toString()}")
 
             //sftp .qcow2 file to the directory on proxmox server
             log.debug("uploading Image $IMAGE_PATH_PREFIX/$imageFile to $hvNode.sshHost:$REMOTE_IMAGE_DIR")
-            ProxmoxMiscUtil.sftpUpload(hvNode.sshHost, 22, hvNode.sshUsername, hvNode.sshPassword, "$IMAGE_PATH_PREFIX/$imageFile", REMOTE_IMAGE_DIR, null)
+                ProxmoxMiscUtil.sftpUpload(hvNode.sshHost, 22, sanitizeSshUsername(hvNode.sshUsername), hvNode.sshPassword, "$IMAGE_PATH_PREFIX/$imageFile", REMOTE_IMAGE_DIR, null)
 
             //create blank vm template on proxmox
             ServiceResponse templateResp = ProxmoxApiComputeUtil.createImageTemplate(client, authConfig, virtualImage.name, hvNode.externalId, 1, 1024L)
@@ -68,12 +75,12 @@ class ProxmoxSshUtil {
             //import the disk file to the blank vm template
             String fileName = new File("$imageFile").getName()
             log.debug("Executing ImportDisk command on node: qm importdisk $imageExternalId $REMOTE_IMAGE_DIR/$fileName $targetDS")
-            def diskCreateOut = context.executeSshCommand(hvNode.sshHost, 22, hvNode.sshUsername, hvNode.sshPassword, "qm importdisk $imageExternalId $REMOTE_IMAGE_DIR/$fileName $targetDS", "", "", "", false, LogLevel.info, true, null, false).blockingGet()
+                def diskCreateOut = context.executeSshCommand(hvNode.sshHost, 22, sanitizeSshUsername(hvNode.sshUsername), hvNode.sshPassword, "qm importdisk $imageExternalId $REMOTE_IMAGE_DIR/$fileName $targetDS", "", "", "", false, LogLevel.info, true, null, false).blockingGet()
             log.debug("Disk ImportDisk SSH Task \"qm importdisk $imageExternalId $REMOTE_IMAGE_DIR/$fileName $targetDS\" results: ${diskCreateOut.toMap().toString()}")
 
             //Mount the disk
             log.debug("Executing DiskMount SSH Task \"qm set $imageExternalId --scsi0 $targetDS:vm-$imageExternalId-disk-0\"")
-            def diskMountOut = context.executeSshCommand(hvNode.sshHost, 22, hvNode.sshUsername, hvNode.sshPassword, "qm set $imageExternalId --scsi0 $targetDS:vm-$imageExternalId-disk-0", "", "", "", false, LogLevel.info, true, null, false).blockingGet()
+                def diskMountOut = context.executeSshCommand(hvNode.sshHost, 22, sanitizeSshUsername(hvNode.sshUsername), hvNode.sshPassword, "qm set $imageExternalId --scsi0 $targetDS:vm-$imageExternalId-disk-0", "", "", "", false, LogLevel.info, true, null, false).blockingGet()
             log.debug("Disk Mount SSH Task \"qm set $imageExternalId --scsi0 $targetDS:vm-$imageExternalId-disk-0\" results: ${diskMountOut.toMap().toString()}")
         } finally {
             context.releaseLock(lockKey, [lock:lock]).blockingGet()
@@ -83,10 +90,11 @@ class ProxmoxSshUtil {
 
 
     private static runSshCmd(MorpheusContext context, ComputeServer hvNode, String cmd) {
-        TaskResult result = context.executeSshCommand(hvNode.sshHost, 22, hvNode.sshUsername, hvNode.sshPassword, cmd, "", "", "", false, LogLevel.info, true, null, false).blockingGet()
-        if (!result.success) {
-            throw new Exception(result.toMap())
-        }
+            String sshUser = sanitizeSshUsername(hvNode.sshUsername)
+            TaskResult result = context.executeSshCommand(hvNode.sshHost, 22, sshUser, hvNode.sshPassword, cmd, "", "", "", false, LogLevel.info, true, null, false).blockingGet()
+            if (!result.success) {
+                throw new Exception(result.toMap())
+            }
     }
 
 }
